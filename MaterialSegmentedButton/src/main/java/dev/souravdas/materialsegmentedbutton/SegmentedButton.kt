e685b94a4af3f78267fc62afe65e3cbe33ecbc8f
Sourav Das <esarve.srv@gmail.com>
2023-05-08 13:46:10 +0600

    initial commit

---
+++ MaterialSegmentedButton/src/main/java/dev/souravdas/materialsegmentedbutton/SegmentedButton.kt
@@ -0,0 +1,105 @@
+package dev.souravdas.materialsegmentedbutton
+
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.offset
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.OutlinedButton
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.zIndex
+
+@Composable
+fun SegmentedButton(
+    items: List<SegmentedButtonItem>,
+    defaultSelectedItemIndex: Int = 0,
+    buttonPadding: PaddingValues = SegmentedButtonDefaults.segmentedButtonPadding(),
+    cornerRadius: SegmentedButtonCorners = SegmentedButtonDefaults.segmentedButtonCorners(),
+    color: SegmentedButtonColors = SegmentedButtonDefaults.segmentedButtonColors(),
+    showIconForSelectedOnly: Boolean = false,
+    modifier: Modifier = Modifier.fillMaxWidth(),
+    onItemSelection: (selectedItemIndex: Int) -> Unit,
+) {
+    val selectedIndex = remember { mutableStateOf(defaultSelectedItemIndex) }
+
+    Row(
+        modifier = modifier
+    ) {
+        val weight = 10.0f / items.size.toFloat()
+        items.forEachIndexed { index, item ->
+            OutlinedButton(
+                modifier = when (index) {
+                    0 -> {
+                        Modifier
+                            .weight(weight)
+                            .offset(0.dp, 0.dp)
+                            .zIndex(1f)
+                    }
+
+                    else -> {
+                        Modifier
+                            .weight(weight)
+                            .offset((-1 * index).dp, 0.dp)
+                            .zIndex(if (selectedIndex.value == index) 1f else 0f)
+                    }
+                },
+                onClick = {
+                    selectedIndex.value = index
+                    onItemSelection(selectedIndex.value)
+                },
+                shape = when (index) {
+                    0 -> RoundedCornerShape(
+                        topStartPercent = cornerRadius.topStartPercentage,
+                        topEndPercent = 0,
+                        bottomStartPercent = cornerRadius.bottomStartPercentage,
+                        bottomEndPercent = 0
+                    )
+
+                    items.size - 1 -> RoundedCornerShape(
+                        topStartPercent = 0,
+                        topEndPercent = cornerRadius.topEndPercentage,
+                        bottomStartPercent = 0,
+                        bottomEndPercent = cornerRadius.bottomEndPercentage
+                    )
+
+                    else -> RoundedCornerShape(
+                        topStartPercent = 0,
+                        topEndPercent = 0,
+                        bottomStartPercent = 0,
+                        bottomEndPercent = 0
+                    )
+                },
+                border = BorderStroke(1.dp, color.outlineColor().value),
+                colors = if (selectedIndex.value == index) {
+                    ButtonDefaults.outlinedButtonColors(
+                        containerColor = color.selectedContainerColor().value,
+                        contentColor = color.selectedContentColor().value
+                    )
+                } else {
+                    ButtonDefaults.outlinedButtonColors(containerColor = color.containerColor().value)
+                },
+                contentPadding = buttonPadding
+            ) {
+                AnimatedVisibility(selectedIndex.value == index && showIconForSelectedOnly) {
+                    CompositionLocalProvider(content = item.leadingIcon)
+                    Spacer(modifier = Modifier.width(4.dp))
+                }
+                if (!showIconForSelectedOnly) {
+                    CompositionLocalProvider(content = item.leadingIcon)
+                    Spacer(modifier = Modifier.width(4.dp))
+                }
+                CompositionLocalProvider(content = item.title)
+            }
+        }
+    }
+}
---
+++ MaterialSegmentedButton/src/main/java/dev/souravdas/materialsegmentedbutton/SegmentedButtonData.kt
@@ -0,0 +1,109 @@
+package dev.souravdas.materialsegmentedbutton
+
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.Immutable
+import androidx.compose.runtime.State
+import androidx.compose.runtime.rememberUpdatedState
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+
+/**
+ * Created by Sourav
+ * On 4/25/2023 11:41 PM
+ * For Hush!
+ */
+
+@Immutable
+class SegmentedButtonColors constructor(
+    private val containerColor: Color,
+    private val contentColor: Color,
+    private val outlineColor: Color,
+    private val selectedContainerColor: Color,
+    private val selectedContentColor: Color,
+    private val selectedLeadingIconColor: Color,
+    private val leadingIconColor: Color,
+) {
+    @Composable
+    fun containerColor(): State<Color> = rememberUpdatedState(containerColor)
+
+    @Composable
+    fun contentColor(): State<Color> = rememberUpdatedState(contentColor)
+
+    @Composable
+    fun outlineColor(): State<Color> = rememberUpdatedState(outlineColor)
+
+    @Composable
+    fun selectedContainerColor(): State<Color> = rememberUpdatedState(selectedContainerColor)
+
+    @Composable
+    fun selectedContentColor(): State<Color> = rememberUpdatedState(selectedContentColor)
+
+    @Composable
+    fun selectedLeadingIconColor(): State<Color> = rememberUpdatedState(selectedLeadingIconColor)
+
+    @Composable
+    fun leadingIconColor(): State<Color> = rememberUpdatedState(leadingIconColor)
+}
+
+object SegmentedButtonDefaults {
+    private const val CORNER_DEFAULT = 16
+    val ITEM_WIDTH = 120.dp
+
+    @Composable
+    fun segmentedButtonColors(
+        containerColor: Color = Color.Transparent,
+        contentColor: Color = MaterialTheme.colorScheme.primary,
+        outlineColor: Color = MaterialTheme.colorScheme.primary,
+        selectedContainerColor: Color = MaterialTheme.colorScheme.primary,
+        selectedContentColor: Color = MaterialTheme.colorScheme.onPrimary,
+        selectedLeadingIconColor: Color = MaterialTheme.colorScheme.onPrimary,
+        leadingIconColor: Color = MaterialTheme.colorScheme.onPrimary
+    ): SegmentedButtonColors = SegmentedButtonColors(
+        contentColor = contentColor,
+        containerColor = containerColor,
+        outlineColor = outlineColor,
+        selectedContentColor = selectedContentColor,
+        selectedContainerColor = selectedContainerColor,
+        selectedLeadingIconColor = selectedLeadingIconColor,
+        leadingIconColor = leadingIconColor
+    )
+
+    fun segmentedButtonPadding(): PaddingValues = PaddingValues(4.dp)
+
+    fun segmentedButtonCorners(): SegmentedButtonCorners = SegmentedButtonCorners(
+        topStartPercentage = CORNER_DEFAULT,
+        topEndPercentage = CORNER_DEFAULT,
+        bottomStartPercentage = CORNER_DEFAULT,
+        bottomEndPercentage = CORNER_DEFAULT
+    )
+
+}
+
+
+class SegmentedButtonItem constructor(
+    val leadingIcon: @Composable () -> Unit,
+    val title: @Composable () -> Unit
+)
+
+class SegmentedButtonCorners {
+    val topStartPercentage: Int
+    val topEndPercentage: Int
+    val bottomStartPercentage: Int
+    val bottomEndPercentage: Int
+
+    constructor(topStartPercentage: Int, topEndPercentage: Int, bottomStartPercentage: Int, bottomEndPercentage: Int) {
+        this.topStartPercentage = topStartPercentage
+        this.topEndPercentage = topEndPercentage
+        this.bottomStartPercentage = bottomStartPercentage
+        this.bottomEndPercentage = bottomEndPercentage
+    }
+
+    constructor(percentage: Int) {
+        this.topStartPercentage = percentage
+        this.topEndPercentage = percentage
+        this.bottomStartPercentage = percentage
+        this.bottomEndPercentage = percentage
+    }
+}
